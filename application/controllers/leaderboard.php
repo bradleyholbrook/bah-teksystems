5f898eb9810cf71a932c8e1391792086e048c10f
Bradley Holbrook <bradley.holbrook@playerspectrum.com>
2013-06-04 16:00:56 -0700

    Removed Default, Added Leaderboard Controller, set as Default

---
+++ application/controllers/leaderboard.php
@@ -0,0 +1,14 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+  
+class Leaderboard extends CI_Controller
+{
+	
+	public function index()
+	{
+		$this->load->view('leaderboard');
+	}
+	
+}
+
+/* End of file leaderboard.php */
+/* Location: ./application/controllers/leaderboard.php */
---
+++ application/views/leaderboard.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="pragma" content="no-cache">
+	<meta http-equiv="cache-control" content="no-cache">
+	<meta http-equiv="content-language" content="en">
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+	<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+	<meta name="generator" content="PhpED Version 6.1 (Build 6118)">
+	<title>Leaderboard</title>
+	<meta name="description" content="Replication of Leadboard Application">
+	<meta name="author" content="Bradley Holbrook">
+	<meta http-equiv="reply-to" content="bholbrook73@gmail.com">
+	<meta name="creation-date" content="06/04/2013">
+	<link rel="stylesheet" type="text/css" href="/assets/css/style.css"/>
+</head>
+<body>
+	
+	<div id="outer">
+		<div class="leaderboard">
+			
+		</div>
+
+		<div class="none">Click a player to select</div>
+		
+	</div>
+	
+</body>
+</html>
